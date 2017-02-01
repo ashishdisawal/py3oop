e4fb8747658bfb1cc743ec22ec3269318da41289
Ashish Disawal <ashish.disawal@gmail.com>
2017-02-01 07:52:05 +0530

    added move and calculate distance methods

--- point.py
+++ point.py
@@ -1,7 +1,19 @@
+import math
+
 class Point:
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+
     def reset(self):
         self.x = 0
         self.y = 0
+
+    def calculate_distance(self, other_point):
+        return math.sqrt(
+                (self.x - other_point.x)**2 +
+                (self.y - other_point.y)**2)
 
 p1 = Point()
 p2 = Point()
@@ -20,3 +32,6 @@ p2.y = 1
 
 print(p1.x, p1.y)
 print(p2.x, p2.y)
+
+print("Distance between p1 and p2: {}".format(p1.calculate_distance(p2)))
+assert (p1.calculate_distance(p2) == p2.calculate_distance(p1))
